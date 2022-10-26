1d885d88aa1e168c441314f5aeb1fc69d8493417
viniciusnd <vinicius.santosnd@gmail.com>
2022-10-26 20:13:32 -0300

    first commit

---
+++ src/Homepage.jsx
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+let alfabeto = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'X', 'W', 'Y', 'Z']
+
+
+export default function Homepage() {
+  const [count, sorter] =
+    useState(function generateRandomLetter() {
+      return (
+
+        <ion-icon name="help-outline"></ion-icon>
+
+      );
+    });
+  function clickOnSort(e) {
+    let arraySorteio = Math.floor(Math.random() * alfabeto.length)
+    let sorteio = alfabeto[arraySorteio]
+    alfabeto.splice(arraySorteio, 1)
+    console.log(alfabeto)
+    sorter(sorteio)
+    if (sorteio === undefined) {
+      sorter("Game Over")
+    }
+  }
+  function ClickonReset(e){
+    if (alfabeto.length < 26){
+      alfabeto = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'X', 'W', 'Y', 'Z']
+      console.log(alfabeto)
+      sorter(<ion-icon name="help-outline"></ion-icon>)
+
+    } else {
+      sorter ("Não é possivel Resetar")
+    }
+  }
+  return (
+    <div className="min-h-screen bg-blue-700">
+      <div className="flex flex-row justify-center">
+        <header className="text-3xl md:text-5xl uppercase mt-5 font-bold font-shadows text-white tracking-widest">Letter Sorter</header>
+      </div>
+
+      <div className="flex flex-row justify-center items-center mt-24">
+        <h1 className="text-5xl mt-5 font-bold font-amiri">{count}</h1>
+      </div>
+
+      <div className="flex flex-row justify-center mt-40 space-x-3.5">
+        <button onClick={clickOnSort} className="bg-purple-400 border rounded-xl px-8 py-3 font-bold font-josefin uppercase ease-in-out duration-300 hover:bg-purple-700 hover:scale-110">Sort</button>
+        <button onClick={ClickonReset} className="bg-purple-400 border rounded-xl px-8 py-3 font-bold font-josefin uppercase ease-in-out duration-300 hover:bg-purple-700 hover:scale-110">Reset</button>
+      </div>
+    </div>
+  )
+}
